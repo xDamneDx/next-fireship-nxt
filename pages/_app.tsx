8b4adb57688e8bc7e1fd0efb28fd86b69b6424de
xDamneDx <xdamnedx@gmail.com>
2022-08-15 16:39:40 +0300

    Added react-hot-toast container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 // Components:
 import Navbar from "../components/Navbar";
@@ -7,6 +8,7 @@ import Navbar from "../components/Navbar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Toaster />
       <Navbar />
       <Component {...pageProps} />;
     </>
