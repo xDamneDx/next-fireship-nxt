de7d7520f9699c5892a987430d24791878b0195e
xDamneDx <xdamnedx@gmail.com>
2022-08-16 15:58:03 +0300

    Added sign in w/ popup

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,55 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import Image from "next/image";
+
 export default function Enter() {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <main>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
+    </main>
   );
+}
+
+// Sign in with Google button:
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <div>
+        <Image
+          src={"/google.png"}
+          alt="google logo"
+          width="30px"
+          height="30px"
+        />
+      </div>
+      Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => signOut(auth)}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
