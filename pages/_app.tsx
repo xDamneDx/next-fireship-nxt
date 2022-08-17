5d9e8292607b076097523e03a720aa887d3bf63c
xDamneDx <xdamnedx@gmail.com>
2022-08-17 11:14:08 +0300

    Added useUserData custom hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserContext } from "../lib/context";
 import { Toaster } from "react-hot-toast";
+import { useUserData } from "../lib/hooks";
 
 // Components:
 import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const userData = useUserData();
+
   return (
     <>
-      <UserContext.Provider value={{ user: {}, username: "xDamneDx" }}>
+      <UserContext.Provider value={userData}>
         <Toaster />
         <Navbar />
         <Component {...pageProps} />;
