24341f1f8a153efc9f48c6e96fe8538e3a31a759
xDamneDx <xdamnedx@gmail.com>
2022-08-19 19:38:45 +0300

    Added UserProfile & PostFeed components. Added gerServerSideProps w/ all users posts from firestore.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,80 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import {
+  collectionGroup,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  startAfter,
+  limit,
+} from "firebase/firestore";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 
-const Home: NextPage = () => {
-  return <div></div>;
+// Components:
+import PostFeed from "../components/PostFeed";
+import Loader from "../components/Loader";
+
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps() {
+  const postsQuery = query(
+    collectionGroup(firestore, "posts"),
+    where("published", "==", true),
+    orderBy("createdAt", "desc"),
+    limit(LIMIT)
+  );
+
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+const Home: NextPage = (props) => {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const q = query(
+      collectionGroup(firestore, "posts"),
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      startAfter(cursor),
+      limit(LIMIT)
+    );
+
+    const newPosts = (await getDocs(q)).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
+  return (
+    <main>
+      <PostFeed posts={posts} />
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+      <Loader show={loading} />
+      {postsEnd && "You have reached the end!"}
+    </main>
+  );
 };
 
 export default Home;
