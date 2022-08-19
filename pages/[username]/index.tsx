de32c82d6dcde2a3c5a7aa1a669c51879e83c9b9
xDamneDx <xdamnedx@gmail.com>
2022-08-19 17:19:19 +0300

    Added UserProfile & PostFeed components. Added gerServerSideProps w/ user posts from firestore.

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,3 +1,50 @@
-export default function UserProfilePage() {
-  return <div>UserProfilePage</div>;
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+// Components:
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+} from "firebase/firestore";
+import { firestore } from "../../lib/firebase";
+
+export async function getServerSideProps({ query: _query }) {
+  const { username } = _query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsColRef = collection(firestore, `users/${userDoc.id}/posts`);
+    const q = query(
+      postsColRef,
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      limit(5)
+    );
+    posts = (await getDocs(q)).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
 }
