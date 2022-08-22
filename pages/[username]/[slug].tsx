fe9a74159ed1e1932f20e2ff3613d65821ace2db
xDamneDx <xdamnedx@gmail.com>
2022-08-22 11:01:42 +0300

    Added ISR w/ getStaticProps & getStaticPaths

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,3 +1,46 @@
-export default function PostPage() {
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { collectionGroup, getDoc, doc, getDocs } from "firebase/firestore";
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = doc(firestore, `users/${userDoc.id}/posts/${slug}`);
+    post = postToJSON(await getDoc(postRef));
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const snapshot = await getDocs(collectionGroup(firestore, "posts"));
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export default function PostPage(props) {
   return <div>PostPage</div>;
 }
