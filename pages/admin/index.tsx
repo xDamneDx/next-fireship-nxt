3b408380bb03c26b4f4805d76ec066e7e1ae23e6
xDamneDx <xdamnedx@gmail.com>
2022-08-23 10:22:14 +0300

    Added AuthCheck component

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,9 @@
-export default function AdminPostsPage() {
-  return <div>AdminPostsPage</div>;
+import AuthCheck from "../../components/AuthCheck";
+
+export default function AdminPostsPage(props) {
+  return (
+    <main>
+      <AuthCheck></AuthCheck>
+    </main>
+  );
 }
