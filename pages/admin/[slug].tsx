8c72461326c3f7df95e21eec8d0fa6ff6ca8ac56
xDamneDx <xdamnedx@gmail.com>
2022-08-23 14:36:53 +0300

    Added form w/ react-hook-form to edit posts in markdown

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,3 +1,111 @@
-export default function AdminPostsEdit() {
-  return <div>AdminPostsEdit</div>;
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import toast from "react-hot-toast";
+import { doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import { firestore, auth } from "../../lib/firebase";
+
+// Styles:
+import styles from "../../styles/Admin.module.css";
+
+// Components:
+import AuthCheck from "../../components/AuthCheck";
+
+export default function AdminPostEdit() {
+  return (
+    <AuthCheck>
+      <PostManager />
+    </AuthCheck>
+  );
+}
+
+function PostManager() {
+  const [preview, setPreview] = useState(false);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const postRef = doc(
+    firestore,
+    `users/${auth.currentUser?.uid}/posts/${slug}`
+  );
+  const [post] = useDocumentData(postRef);
+
+  return (
+    <main className={styles.container}>
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            <PostForm
+              postRef={postRef}
+              defaultValues={post}
+              preview={preview}
+            />
+          </section>
+
+          <aside>
+            <h3>Tools</h3>
+            <button onClick={() => setPreview(!preview)}>
+              {preview ? "Edit" : "Preview"}
+            </button>
+            <Link href={`/${post.username}/${post.slug}`}>
+              <button className="btn-blue">Live view</button>
+            </Link>
+          </aside>
+        </>
+      )}
+    </main>
+  );
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+  const { register, handleSubmit, reset, watch } = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const updatePost = async ({ content, published }) => {
+    await updateDoc(postRef, {
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    });
+
+    reset({ content, published });
+
+    toast.success("Post updated successfully!");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch("content")}</ReactMarkdown>
+        </div>
+      )}
+
+      <div className={preview ? styles.hidden : styles.controls}>
+        <textarea {...register("content")}></textarea>
+
+        <fieldset>
+          <input
+            className={styles.checkbox}
+            type="checkbox"
+            {...register("published")}
+          />
+          <label>Published</label>
+        </fieldset>
+
+        <button type="submit" className="btn-green">
+          Save Changes
+        </button>
+      </div>
+    </form>
+  );
 }
