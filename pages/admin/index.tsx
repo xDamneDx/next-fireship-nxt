bc797ec27f896de9f076cb47829c0c373da66693
xDamneDx <xdamnedx@gmail.com>
2022-08-23 13:37:18 +0300

    Added functionality for creating a new post doc in Firestore with a custom slug id

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,9 +1,104 @@
-import AuthCheck from "../../components/AuthCheck";
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import kebabCase from "lodash.kebabcase";
+import toast from "react-hot-toast";
+import { useCollection } from "react-firebase-hooks/firestore";
+import {
+  serverTimestamp,
+  collection,
+  doc,
+  setDoc,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { UserContext } from "../../lib/context";
+import { firestore, auth } from "../../lib/firebase";
 
-export default function AdminPostsPage(props) {
+// Styles:
+import styles from "../../styles/Admin.module.css";
+
+// Components:
+import AuthCheck from "../../components/AuthCheck";
+import PostFeed from "../../components/PostFeed";
+
+export default function AdminPostsPage() {
   return (
     <main>
-      <AuthCheck></AuthCheck>
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
     </main>
+  );
+}
+
+function PostList() {
+  const ref = collection(firestore, `users/${auth.currentUser?.uid}/posts`);
+  const q = query(ref, orderBy("createdAt"));
+  const [querySnapshot] = useCollection(q);
+
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  // Ensure slug is URL safe
+  const slug = encodeURI(kebabCase(title));
+
+  // Validate length
+  const isValid = title.length > 3 && title.length < 100;
+
+  // Create a new post in firestore
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser?.uid;
+    const ref = doc(firestore, `users/${uid}/posts/${slug}`);
+
+    // Tip: give all fields a default value here
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: "# hello world!",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await setDoc(ref, data);
+
+    toast.success("Post created!");
+
+    // Imperative navigation after doc is set
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome Article!"
+        className={styles.input}
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Create New Post
+      </button>
+    </form>
   );
 }
