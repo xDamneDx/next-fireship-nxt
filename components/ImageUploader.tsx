07e50cae88cd29bc216dc587287014d0b0f772ad
xDamneDx <xdamnedx@gmail.com>
2022-08-25 10:46:16 +0300

    Created ImageUploader component

---
+++ components/ImageUploader.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { auth, firestore } from "../lib/firebase";
+
+// Components:
+import Loader from "./Loader";
+
+// Uploads images to FIrebase Storage:
+export default function ImageUploader() {
+  const [uploading, setUploading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [downloadURL, setDownloadURL] = useState(null);
+
+  return (
+    <div className="box">
+      <Loader show={uploading} />
+      {uploading && <h3>{progress}%</h3>}
+
+      {!uploading && (
+        <>
+          <label className="btn">
+            📸 Upload Img
+            <input
+              type="file"
+              onChange={uploadFile}
+              accept="image/x-png,image/gif,image/jpeg"
+            />
+          </label>
+        </>
+      )}
+
+      {downloadURL && (
+        <code className="upload-snippet">{`![alt](${downloadURL})`}</code>
+      )}
+    </div>
+  );
+}
